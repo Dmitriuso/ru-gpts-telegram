8e7e5a6b34c8cc6d4f64cc2480ec5bdfcd04c0f8
Dmitri Osipov <dmitri.osipov@lexistems.com>
2021-01-03 21:57:39 +0300

    short bot for antosha

--- telegram/short_bot.py
+++ telegram/short_bot.py
@@ -13,7 +13,7 @@ def message_handler(update: Update, context: CallbackContext):
                                     "/Users/dmitriosipov/my_research/deep_learning/text_generation/telegram-chat-bot/ru-gpts-telegram/generate_transformers.py "
                                     "--model_type=gpt2 "
                                     "--model_name_or_path=/Users/dmitriosipov/my_research/deep_learning/text_generation/telegram-chat-bot/ru-gpts-telegram/model_from_web/lines_1024_batches "
-                                    "--k=20 --p=0.9 --prompt='{}' --length=100".format(my_text), shell=True, capture_output=True)
+                                    "--k=20 --p=0.9 --prompt='{}' --length=150".format(my_text), shell=True, capture_output=True)
     decoded_generated_text = generated_text.stdout.decode()
     response = post_processing(str(decoded_generated_text))
     update.message.reply_text(text=str(response))
@@ -23,7 +23,7 @@ def message_handler(update: Update, context: CallbackContext):
 def main():
     print("Start")
     updater = Updater(
-        token="1177059750:AAHMtxoYvt71kF_TWhvxJsTdN4dFV6a0Iag",
+        token="1487513251:AAGuqREy6i6xqATYxrwN9DvlWAJS2ioBk3M",
         use_context=True,
     )
     updater.dispatcher.add_handler(MessageHandler(filters=Filters.text, callback=message_handler))
